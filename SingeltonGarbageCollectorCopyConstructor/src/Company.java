eacf7301cf78a6e990f73f4aa435dfe49ea6ed7d
Nicolai <nihermann@uos.de>
2021-04-26 19:52:01 +0200

    added comments

--- SingeltonGarbageCollectorCopyConstructor/src/Company.java
+++ SingeltonGarbageCollectorCopyConstructor/src/Company.java
@@ -1,30 +1,30 @@
 /**
- *
+ * @Author Nicolai Hermann, Michael Hüppe.
  */
 public class Company {
     /**
-     *
+     * The name of the company.
      */
     private String name;
 
     /**
-     *
-     * @param name
+     * Constructs a company with given name.
+     * @param name string - name of the company.
      */
     public Company(String name){
         this.name = name;
     }
 
     /**
-     *
-     * @param d
+     * Changes the stock price of the company which will also be logged.
+     * @param d double - the new value of the company's stock price.
      */
     public void changeStockPrice(double d){
         Ticker.getTicker().print("\n" + this.name + " " + d);
     }
 
     /**
-     *
+     * If the company is deleted it will get logged as the company is insolvent.
      */
     public void finalize(){
         Ticker.getTicker().print("\n" + this.name + " is insolvent");
