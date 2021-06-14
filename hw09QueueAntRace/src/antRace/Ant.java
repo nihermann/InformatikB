b6c71ee50e86a7d7a4cd811152d5584b593e5e3b
Nicolai <nihermann@uos.de>
2021-06-15 00:25:44 +0200

    setup hw09

---
+++ hw09QueueAntRace/src/antRace/Ant.java
@@ -0,0 +1,47 @@
+package antRace;
+
+import antRace.AntField.Field;
+
+/**
+ * An {@code Ant} is created at a specific position of an {@link AntField} with
+ * an initial {@code stepCount}. When running an Ant, it will lookup the values
+ * on the current and all surrounding {@link Field}
+ * (Moore-neighborhood) instances and test if the position is free, i.e. has a
+ * value of {@code 0}, or if the value is greater than the {@code stepCount} of
+ * this Ant. For both cases, the Ant will set the value of the {@code Field} at
+ * the visited position to its own {@code stepCount+1}. After an {@code Ant} has
+ * successfully visited one field, it will create new {@code Ant} instances with
+ * an incremented {@code stepCount} to visit the other available {@code Field}
+ * elements. The Ant will run until it finds no more {@code Field} elements in
+ * its neighborhood to be altered.
+ * 
+ * @author Mathias Menninghaus (mathias.menninghaus@uos.de)
+ * 
+ */
+public class Ant implements Runnable {
+
+
+   /**
+    * 
+    * @param fields
+    *           the {@code AntField} on which this {@code Ant} operates
+    * @param x
+    *           x-axis value of the starting position
+    * @param y
+    *           y-axis value of the starting position
+    * @param stepCount
+    *           initial stepCount of this {@code Ant}.
+    * 
+    * @throws IllegalArgumentException
+    *            If the {@code Field} at position {@code x,y} does not exist, or
+    *            if its value is < 0
+    */
+   public Ant(AntField fields, int x, int y, int stepCount) {
+   
+   }
+
+   public void run() {
+      
+   }
+
+}
