9306bd0d4f2926907b1eaf1a0e43d09eb2c368ba
Nicolai <nihermann@uos.de>
2021-06-21 19:12:12 +0200

    used thread group instead of shutdownhook

--- hw09QueueAntRace/src/antRace/AntRace.java
+++ hw09QueueAntRace/src/antRace/AntRace.java
@@ -9,8 +9,30 @@ public class AntRace implements AntFields {
 
 		Ant ant = new Ant(field, 2, 4, 1);
 
-		new Thread(ant).start();
+		ThreadGroup t = new ThreadGroup("Ants");
 
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println(field)));
+		new Thread(t, ant).start();
+
+		waitForGroupToFinish(t);
+
+		System.out.println(field);
+
+		//Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println(field)));
+	}
+
+	/**
+	 * Waits for all threads of a certain {@link ThreadGroup} {@code t} to finish.
+	 * @param t the {@link ThreadGroup} to be waited for.
+	 */
+	private static void waitForGroupToFinish(ThreadGroup t) {
+		synchronized(t){
+			while(t.activeCount() > 0){
+				try {
+					t.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
